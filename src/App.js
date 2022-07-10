d4ecdcab894c497428c1039fb74d51f35c570a93
lemonmeringue93 <100950641+lemonmeringue93@users.noreply.github.com>
2022-07-10 17:55:27 +0100

    fixed css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
   <div className="App">
     <div className="container">
-  <h1>  Weather App</h1>
-  <footer>
+        <Weather />
+        <footer>
   This project is coded by Belle Zamora and is <a href="https://github.com/lemonmeringue93/react-weather-app2" target="_blank" rel="noreferrer"> open-sourced on Github
   </a>
   </footer>
