784d264d638c107ed57c0e0af8c1ae398c27133a
lemonmeringue93 <100950641+lemonmeringue93@users.noreply.github.com>
2022-07-10 16:43:03 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Weather App
-      <footer>
-        This project is coded by Belle Zamora and is <a href="https://github.com/lemonmeringue93/react-weather-app2" target="_blank" rel="noreferrer"> open-sourced on Github
-    </a>
-        </footer>
-        </div>
+  <div className="App">
+    <div className="container">
+  <h1>  Weather App</h1>
+  <footer>
+  This project is coded by Belle Zamora and is <a href="https://github.com/lemonmeringue93/react-weather-app2" target="_blank" rel="noreferrer"> open-sourced on Github
+  </a>
+  </footer>
+  </div>
+  </div>
   );
 }
 
