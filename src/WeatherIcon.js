c829a7d52a4d966bcff1d61c7ddeacb3da543bcd
lemonmeringue93 <100950641+lemonmeringue93@users.noreply.github.com>
2022-08-26 11:39:54 +0100

    added animated icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,9 +1,11 @@
-import { prependOnceListener } from "bootstrapp";
+
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
+
 export default function WeatherIcon(props) {
-    const codeMapping ={
-        "01d": "CLEARDAY",
+    console.log(props.code);
+    const codeMapping = {
+        "01d": "CLEAR_DAY",
         "01n": "CLEAR_NIGHT",
         "02d": "PARTLY_CLOUDY_DAY",
         "02n": "PARTLY_CLOUDY_NIGHT",
@@ -14,7 +16,7 @@ export default function WeatherIcon(props) {
         "09d": "RAIN",
         "09n": "RAIN",
         "010d": "RAIN",
-        "010n": "CLEAR_NIGHT",
+        "010n": "RAIN",
         "011d": "CLEAR_NIGHT",
         "011n": "CLEAR_NIGHT",
         "013d": "SNOW",
@@ -22,9 +24,8 @@ export default function WeatherIcon(props) {
         "015d": "FOG",
         "015n": "FOG"
     };
-    
-    if (props.code == "01d") {
-        return (
+
+    return (
             <ReactAnimatedWeather
             icon={codeMapping[props.code]}
             color="#1d6ef8"
@@ -33,5 +34,5 @@ export default function WeatherIcon(props) {
             />
         );
 }
-}
+
 
