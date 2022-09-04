45906aa87b8bf3383b277f835ec18a54ee6c5fb5
lemonmeringue93 <100950641+lemonmeringue93@users.noreply.github.com>
2022-09-04 11:31:17 +0100

    saved

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+    
     let [loaded, setLoaded] = useState(false)
     let [forecast, setForecast] = useState(null);
 
@@ -15,18 +16,13 @@ export default function WeatherForecast(props) {
     if (loaded) {
         console.log(forecast);
         return (
-         <div className="WeatherForecast">
+            <div className="WeatherForecast">
             <div className="row">
             <div className="col">
-                <div className="WeatherForecast-day">Thu</div> 
-                    <WeatherIcon code="01d" size={32}/>
-                    <div className="WeatherForecast-temperature"> 
-                    <span className="WeatherForecast-temperature-max">{forecast[0].temp.max}°</span>
-                    <span className="WeatherForecast-temperature-min">{forecast[0].temp.min}°</span> 
-                    </div>
+                <WeatherForecastDay data={forecast[0]} />
             </div>
             </div>
-        </div>
+            </div>
         );
 
     } else { 
